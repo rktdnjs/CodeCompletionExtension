8e50fdfc90e72fe6aaa2f98a1dd81b0ba686915e
rktdnjs <rktdnjs@naver.com>
2024-05-09 20:42:45 +0900

    [feat] : 버그 수정 완료

--- src/extension.ts
+++ src/extension.ts
@@ -14,9 +14,9 @@ export function activate(context: vscode.ExtensionContext) {
   let currentDocument: vscode.TextDocument | undefined = undefined;
   let disposable = vscode.commands.registerCommand("extension.completeCode", async () => {
     const folderPath = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
-    const userEditor = vscode.window.activeTextEditor;
     const untitledUri = vscode.Uri.parse("untitled:" + path.join("SuggestedCode.sb"));
     const document = await vscode.workspace.openTextDocument(untitledUri);
+    const userEditor = vscode.window.activeTextEditor;
     const newEditor = await vscode.window.showTextDocument(document, {
       viewColumn: vscode.ViewColumn.Beside,
       preview: false,
@@ -30,12 +30,16 @@ export function activate(context: vscode.ExtensionContext) {
     if (userEditor) {
       const document = userEditor.document;
       const entireText = document.getText(); // 문서의 전체 내용을 가져옵니다.
-      console.log("Current document text:", entireText); // 콘솔에 현재 문서의 내용 출력
 
       // 정보 메시지 표시 (지연 처리 포함)
       vscode.window.showInformationMessage("ChatGPT SmallBasic Completion is generating code...", "OK");
       setTimeout(async () => {
         await newEditor.edit((editBuilder) => {
+          // 웹뷰의 기존 내용을 전부 삭제(초기화)
+          const lastLine = newEditor.document.lineAt(newEditor.document.lineCount - 1);
+          const range = new vscode.Range(new vscode.Position(0, 0), lastLine.range.end);
+          editBuilder.delete(range);
+
           editBuilder.insert(
             new vscode.Position(0, 0),
             "[입력한 코드]\n" +
